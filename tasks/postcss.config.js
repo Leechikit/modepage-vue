f8514f1406cbbcf2c4082adb6a374204a46773da
李子杰 <leechikit0823@163.com>
2017-11-15 15:49:01 +0800

    update

--- tasks/postcss.config.js
+++ tasks/postcss.config.js
@@ -2,11 +2,11 @@ module.exports = {
     parser: 'postcss-scss',
     plugins: {
         'postcss-import': {},
-        'postcss-cssnext': {
-            warnForDuplicates: false
-        },
         'postcss-px2rem': {
             remUnit: 75
+        },
+        'postcss-cssnext': {
+            warnForDuplicates: false
         },
         'cssnano': {}
     }
