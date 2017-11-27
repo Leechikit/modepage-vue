cf60e21363e2dca94bd1cdcc6b843a6aad8f680e
李子杰 <leechikit0823@163.com>
2017-11-27 19:00:52 +0800

    update

---
+++ src/components/head-section/head-section.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="navbar" data-modName="modHeader">
+        <div class="w-head" id="wHead">
+            <div class="w-head-main">
+                <a href="/" class="yy-logo"></a>
+                <div class="w-head-download" id="downloadYY">
+                    <a>下载YY</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import './head-section.scss';
+
+    export default {
+        
+    }
+</script>
